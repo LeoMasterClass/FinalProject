73d1b00653d9186f8b47f94088687c21b6b3e2f7
leo lorin <leolorin9@gmail.com>
2020-02-16 22:57:35 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once "layouts/head.php";
+include_once "layouts/header.php";
+?>
+
+
+
+<?php
+include_once "layouts/footer.php";
+include_once "layouts/script.php";
+?>
---
+++ layouts/header.php
@@ -0,0 +1,7 @@
+<body>
+    <header>
+    <div></div>
+    <a href="index.php"><img src="../public/img/index/Bandeaulogo.png" alt=""></a>
+
+
+    </header>
