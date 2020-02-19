a64e77ce3b6701bd14acce26cc8cd04964603d2c
unknown <leolorin9@gmail.com>
2020-02-19 22:12:42 +0100

    index sans footer

--- layouts/head.php
+++ layouts/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="public/img/logo/favicon.jpeg" />
     <title>CatEvents</title>
     <link rel="stylesheet" href="public/css/header.css">
     <link rel="stylesheet" href="public/css/main-index.css">
